296159030ce941a2ff80ff4c8c033c5e813b7e0c
Marcos <marcosdevelopment.business@gmail.com>
2024-06-17 19:20:15 -0300

    feat: support zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 cp .machinesrc $HOME
-echo "source $HOME/.machinesrc" >> $HOME/.bashrc
+
+if [[ "$SHELL" =~ bash$ ]]; then
+  echo "source $HOME/.machinesrc" >> $HOME/.bashrc
+fi
+
+if [[ "$SHELL" =~ zsh$ ]]; then
+  echo "source $HOME/.machinesrc" >> $HOME/.zshrc
+fi
 
 echo -e "\033[1;32mMACHINE RUNNER INSTALLED SUCCESSFULLY\033[0m"
 echo -e "\033[1;36mPLEASE, RESTART YOUR TERMINAL\033[0m"
